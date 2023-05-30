c3da81ba744465c5c43d27b5699ce380503a3c04
KingNelx <tapiajonel606@gmail.com>
2023-05-31 02:20:07 +0800

    added other components

--- front-end-app/src/App.jsx
+++ front-end-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Start from './Components/Start';
+import Home from './Components/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- front-end-app/src/Components/Start.jsx
+++ front-end-app/src/Components/Start.jsx
@@ -1,17 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Start = () => {
     return ( 
-        <div>
-            <div class="container text-center">
+        // <div className="container mt-5">
+        //     <Link to="home"> Get Started </Link>
+        // </div>
+        <div class="container text-center mt-5">
   <div class="row">
-    <div class="col">
-      Column
+    <div class="col align-self-start">
+      One of three columns
     </div>
-    <div class="col">
-      Column
+    <div class="col align-self-center">
+      One of three columns
+    </div>
+    <div class="col align-self-end">
+      One of three columns
     </div>
   </div>
 </div>
-        </div>
      );
 }
  
