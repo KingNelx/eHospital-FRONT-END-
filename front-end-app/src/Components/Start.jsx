a9065f943aa6c788cbe1cb038a92383ec729ada1
KingNelx <tapiajonel606@gmail.com>
2023-05-31 02:36:55 +0800

    added other components

--- front-end-app/src/Components/Start.jsx
+++ front-end-app/src/Components/Start.jsx
@@ -1,24 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Start = () => {
-    return ( 
-        // <div className="container mt-5">
-        //     <Link to="home"> Get Started </Link>
-        // </div>
-        <div class="container text-center mt-5">
-  <div class="row">
-    <div class="col align-self-start">
-      One of three columns
-    </div>
-    <div class="col align-self-center">
-      One of three columns
-    </div>
-    <div class="col align-self-end">
-      One of three columns
-    </div>
-  </div>
-</div>
-     );
+    return (
+        <div class="container text-center">
+            <div class="col align-self-center mt-5">
+                <Link to="home" className="btn btn-outline-primary"> Get Started </Link>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Start;
