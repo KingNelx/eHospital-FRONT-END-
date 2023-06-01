4a4e27805d34af2b1e8b0ed66aa06bb9b4ea0962
KingNelx <tapiajonel606@gmail.com>
2023-06-01 21:22:31 +0800

    test

--- front-end-app/src/Components/Start.jsx
+++ front-end-app/src/Components/Start.jsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router-dom";
-
 const Start = () => {
     return (
-        <div class="container text-center">
-            <div class="col align-self-center mt-5">
-                <Link to="home" className="btn btn-outline-primary"> Get Started </Link>
-            </div>
+        <div className="container mt-5">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 }
